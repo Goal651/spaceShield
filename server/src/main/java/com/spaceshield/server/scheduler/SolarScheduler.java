06efe603c0f3a42fe4294faa6757106d2407aa1e
Goal651 <bugiriwilson651@gmail.com>
2026-06-06 10:47:02 +0200

    feat: integrate NasaApiClient into Fireball and Solar schedulers to fetch and log space event data

--- server/src/main/java/com/spaceshield/server/scheduler/SolarScheduler.java
+++ server/src/main/java/com/spaceshield/server/scheduler/SolarScheduler.java
@@ -1,9 +1,27 @@
 package com.spaceshield.server.scheduler;
 
-import org.springframework.scheduling.annotation.Scheduled;
+import java.time.LocalDate;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.spaceshield.server.client.NasaApiClient;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class SolarScheduler {
+    private final NasaApiClient nasaApiClient;
+    private final String today = LocalDate.now().toString();
+
     @Scheduled(fixedRate = 300000)
     public void fetchSolarEvents() {
+        nasaApiClient.getSolarFlares(today, today)
+                .doOnNext(solarFlare -> log.info("Solar Flare Data: Flare ID={}, Class Type={}", solarFlare.flrId(),
+                        solarFlare.classType()))
+                .subscribe();
     }
 }
